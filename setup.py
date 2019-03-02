5e3023fb725ca2080730fbe78c8a9decdb2c767e
Just4test <myservice@just4test.net>
2019-03-02 09:19:53 +0800

    test AWS Codebuild

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cfn-yaml',
-    version = '0.0.5',
+    version = '0.0.6',
     keywords = ('CloudFormation', 'YAML'),
     description = 'Let you load cloudformation template.yaml',
     url = 'https://github.com/Just4test/cfn-yaml',
